d517cc632141720b3b0214f519e29114426adf00
Shondriane <shondriane.wise@yahoo.com>
2023-10-27 15:46:51 -0700

    update bg color and text for donate button

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -105,7 +105,7 @@ export default function Header() {
                   size="small"
                   className="donate-btn"
                   sx={{
-                    whiteSpace: 'nowrap', bgcolor: '#72C850', py: 1, px: 4,
+                    whiteSpace: 'nowrap', bgcolor: '#72C850', py: 1, px: 4, '&:hover': { color: '#000000', bgcolor: '#72C850' },
                   }}
                 >
                   Donate
